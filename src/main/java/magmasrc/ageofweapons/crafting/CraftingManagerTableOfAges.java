d558e0fa69f3a45f4ae178bc75c3064b1aafd7cc
Steven <steven.zi1207@gmail.com>
2017-05-01 14:28:26 +0200

    Tabs & Recipes

--- src/main/java/magmasrc/ageofweapons/crafting/CraftingManagerTableOfAges.java
+++ src/main/java/magmasrc/ageofweapons/crafting/CraftingManagerTableOfAges.java
@@ -11,8 +11,6 @@ import net.minecraft.init.Items;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.item.crafting.ShapedRecipes;
 import net.minecraft.world.World;
 import net.minecraftforge.oredict.OreDictionary;
 
@@ -23,7 +21,7 @@ public class CraftingManagerTableOfAges {
 
     private static final CraftingManagerTableOfAges INSTANCE = new CraftingManagerTableOfAges();
 
-    private final List<ShapedRecipesTableOfAges> recipes = Lists.<ShapedRecipesTableOfAges>newArrayList();
+    private final List<ShapedRecipesTableOfAges> recipes = Lists.newArrayList();
 
     public static CraftingManagerTableOfAges getInstance()
     {
