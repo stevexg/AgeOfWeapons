6212560ed217537e8a96dc6fe4fbb1e190d47ae0
XxRexRaptorxX <Raptor-Simon@web.de>
2017-05-02 21:38:25 +0200

    item name changes

--- src/main/java/magmasrc/ageofweapons/main/AgeOfWeapons.java
+++ src/main/java/magmasrc/ageofweapons/main/AgeOfWeapons.java
@@ -26,7 +26,7 @@ public class AgeOfWeapons {
    
     public static final String MODID = "ageofweapons";
     public static final String VERSION = "0.1.0";
-    
+    // TODO: Future & BBY texturen und funktionen tauschen
    
     @Instance("ageofweapons")
     public static AgeOfWeapons instance;
