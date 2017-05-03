f5335ee67307e52ae6198aac52da026eb5a5e12b
XxRexRaptorxX <Raptor-Simon@web.de>
2017-05-03 18:41:21 +0200

    table changes and more

--- src/main/java/magmasrc/ageofweapons/blocks/BlockTableOfAges.java
+++ src/main/java/magmasrc/ageofweapons/blocks/BlockTableOfAges.java
@@ -24,6 +24,8 @@ import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -43,6 +45,8 @@ public class BlockTableOfAges extends BlockContainer  {
 		this.setHardness(2.5F);
 		this.setResistance(5.0F);
 		this.setSoundType(SoundType.STONE);		
+		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+
 	}
 
 	
@@ -65,6 +69,25 @@ public class BlockTableOfAges extends BlockContainer  {
 		playerIn.openGui(AgeOfWeapons.instance, GuiHandlerTOA.getGuiID(), worldIn, pos.getX(), pos.getY(), pos.getZ());
 		return true;
 	}
+	
+
+	@Override
+	public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
+
+		TileEntity tileentity = worldIn.getTileEntity(pos);
+
+		if (tileentity instanceof IInventory)
+		{
+			InventoryHelper.dropInventoryItems(worldIn, pos, (IInventory)tileentity);
+			worldIn.updateComparatorOutputLevel(pos, this);
+		}
+
+
+
+		super.breakBlock(worldIn, pos, state);
+
+	}
+	
 
 	
 
@@ -98,52 +121,94 @@ public class BlockTableOfAges extends BlockContainer  {
 		return EnumBlockRenderType.MODEL;
 	}
 	
-	
-	
-	
-	// Facing
-	
-	@Override
-	public IBlockState getStateForPlacement(World world, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer){
-		IBlockState state = super.getStateForPlacement(world, pos, facing, hitX, hitY, hitZ, meta, placer);
-		state = state.withProperty(FACING, placer.getHorizontalFacing());
-	return state;
-	}
-	
-	@Override
-	public int getMetaFromState(IBlockState state)	{
-		return ((EnumFacing) state.getValue(FACING)).getHorizontalIndex();
-	}
-	
-	@Override
-	protected BlockStateContainer createBlockState() {
-	return new BlockStateContainer(this, new IProperty[] { FACING });
-	}
-	
-	
 	@Override
 	public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
 		return null;
 	}
+
 	
-
-	@Override
-	public void breakBlock(World worldIn, BlockPos pos, IBlockState state) {
-
-		TileEntity tileentity = worldIn.getTileEntity(pos);
-
-		if (tileentity instanceof IInventory)
-		{
-			InventoryHelper.dropInventoryItems(worldIn, pos, (IInventory)tileentity);
-			worldIn.updateComparatorOutputLevel(pos, this);
-		}
-
-
-
-		super.breakBlock(worldIn, pos, state);
-
-	}
 	
+	
+	
+		// Facing
+	
+		@Override
+		  public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
+		        this.setDefaultFacing(worldIn, pos, state);
+		    }
+
+
+		    private void setDefaultFacing(World worldIn, BlockPos pos, IBlockState state)
+		    {
+		        if (!worldIn.isRemote)
+		        {
+		            IBlockState iblockstate = worldIn.getBlockState(pos.north());
+		            IBlockState iblockstate1 = worldIn.getBlockState(pos.south());
+		            IBlockState iblockstate2 = worldIn.getBlockState(pos.west());
+		            IBlockState iblockstate3 = worldIn.getBlockState(pos.east());
+		            EnumFacing enumfacing = (EnumFacing)state.getValue(FACING);
+
+		            if (enumfacing == EnumFacing.NORTH && iblockstate.isFullBlock() && !iblockstate1.isFullBlock())
+		            {
+		                enumfacing = EnumFacing.SOUTH;
+		            }
+		            else if (enumfacing == EnumFacing.SOUTH && iblockstate1.isFullBlock() && !iblockstate.isFullBlock())
+		            {
+		                enumfacing = EnumFacing.NORTH;
+		            }
+		            else if (enumfacing == EnumFacing.WEST && iblockstate2.isFullBlock() && !iblockstate3.isFullBlock())
+		            {
+		                enumfacing = EnumFacing.EAST;
+		            }
+		            else if (enumfacing == EnumFacing.EAST && iblockstate3.isFullBlock() && !iblockstate2.isFullBlock())
+		            {
+		                enumfacing = EnumFacing.WEST;
+		            }
+
+		            worldIn.setBlockState(pos, state.withProperty(FACING, enumfacing), 2);
+		        }
+		    }
+		    
+		    
+		    @Override
+		    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer)
+		    {
+		        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+		    }
+		    
+
+		    @Override
+		    public IBlockState getStateFromMeta(int meta){
+		        EnumFacing enumfacing = EnumFacing.getFront(meta);
+
+		        if (enumfacing.getAxis() == EnumFacing.Axis.Y)
+		        {
+		            enumfacing = EnumFacing.NORTH;
+		        }
+
+		        return this.getDefaultState().withProperty(FACING, enumfacing);
+		    }
+
+		    @Override
+		    public int getMetaFromState(IBlockState state) {
+		        return ((EnumFacing)state.getValue(FACING)).getIndex();
+		    }
+
+		    @Override
+		    public IBlockState withRotation(IBlockState state, Rotation rot)
+		    {
+		        return state.withProperty(FACING, rot.rotate((EnumFacing)state.getValue(FACING)));
+		    }
+
+		    @Override
+		    public IBlockState withMirror(IBlockState state, Mirror mirrorIn){
+		        return state.withRotation(mirrorIn.toRotation((EnumFacing)state.getValue(FACING)));
+		    }
+
+		    @Override
+		    protected BlockStateContainer createBlockState() {
+		        return new BlockStateContainer(this, new IProperty[] {FACING});
+		    }
 	
 	
 
