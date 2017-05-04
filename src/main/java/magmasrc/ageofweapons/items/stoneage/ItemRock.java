3983688a28e757d3efd0bd6c6c1454851006f04f
Steven <steven.zi1207@gmail.com>
2017-05-04 19:14:47 +0200

    Item Rock started

---
+++ src/main/java/magmasrc/ageofweapons/items/stoneage/ItemRock.java
@@ -0,0 +1,15 @@
+package magmasrc.ageofweapons.items.stoneage;
+
+import magmasrc.ageofweapons.main.ModTabs;
+import net.minecraft.item.Item;
+
+public class ItemRock extends Item {
+
+    public ItemRock() {
+        super();
+        this.setCreativeTab(ModTabs.stoneAgeTab);
+        this.setMaxStackSize(64);
+    }
+
+
+}
