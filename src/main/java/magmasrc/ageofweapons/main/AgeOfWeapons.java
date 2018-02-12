4fe108ddc1f007698f06e78ff890ca80cb114375
Steven Zimmermann <steven.zi1207@gmail.com>
2018-02-12 13:13:17 +0100

    Version 0.13.1 build

--- src/main/java/magmasrc/ageofweapons/main/AgeOfWeapons.java
+++ src/main/java/magmasrc/ageofweapons/main/AgeOfWeapons.java
@@ -27,7 +27,7 @@ import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 public class AgeOfWeapons {
    
     public static final String MODID = "ageofweapons";
-    public static final String VERSION = "0.13.0";
+    public static final String VERSION = "0.13.1";
 
    
     @Instance("ageofweapons")
